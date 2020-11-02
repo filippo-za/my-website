df613c2535af69647b455824e47103ee6ade2fcf
Filippo Zarba <filippozarba00@gmail.com>
2020-11-02 20:23:33 +0100

    additions

--- index.js
+++ index.js
@@ -9,13 +9,6 @@
     duration: 1000,
     interval: 300,
   });
-  ScrollReveal().reveal("zoom", {
-    duration: 1500,
-    easing: "cubic-bezier(0.215, 0.61, 0.355, 1)",
-    interval: 200,
-    scale: 0.65,
-    mobile: false,
-  });
 
   window.addEventListener("scroll", function (e) {
     var $header = $(".header");
